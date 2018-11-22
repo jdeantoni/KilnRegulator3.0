ba803ccb2e004326a188803e07150cbc05663a1f
piernov <piernov@piernov.org>
2018-11-22 09:47:53 +0100

    NodeJS auto reconnect to Arduino on close/error

--- raspberrypi/services/arduino.js
+++ raspberrypi/services/arduino.js
@@ -2,12 +2,20 @@ var SerialPort = require('serialport');
 
 class Arduino {
   constructor(dev, baudRate) {
+    var arduino = this;
     this.serialPort = new SerialPort(dev, {
       baudRate: baudRate,
       autoOpen: false
     });
     this.serialPort.on('error', function(err) {
       console.log(err);
+      setTimeout(function() {
+        arduino.open();
+      }, 5000);
+    });
+    this.serialPort.on('close', function(err) {
+      console.error('Connection to Arduino closed, ' + err);
+      arduino.open();
     });
   }
 
