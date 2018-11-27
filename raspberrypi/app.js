fb75f1d7bf9a42cf9e27c41bdb104b72fd226dc4
piernov <piernov@piernov.org>
2018-11-27 15:24:20 +0100

    NodeJS: complete CORS support

--- raspberrypi/app.js
+++ raspberrypi/app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
