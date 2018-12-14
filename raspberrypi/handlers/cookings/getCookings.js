d65bb06cc43c70397bfd472998e410ba558fd53d
piernov <piernov@piernov.org>
2018-12-14 10:57:05 +0100

    NodeJS: add program name to getCookings

--- raspberrypi/handlers/cookings/getCookings.js
+++ raspberrypi/handlers/cookings/getCookings.js
@@ -9,6 +9,7 @@
  */
 exports.handler = function getCooking(req, res, next) {
   const cookingRepository = require('../../services/cookingrepository');
+  const programRepository = require('../../services/programrepository');
 
   cookingRepository.all(function(err, cookings) {
     if (err) {
@@ -16,8 +17,29 @@ exports.handler = function getCooking(req, res, next) {
       res.send({error: err});
       console.error(err);
     } else {
-      res.send(cookings);
+      programRepository.all(function(err, programs) {
+        if (err) {
+          res.status(500);
+          res.send({error: err});
+          console.error(err);
+        } else {
+          let programName = {};
+          //map program uuid to program name
+          programs.forEach((program) => {
+            programName[program.uuid] = program.name;
+          });
+
+          //add program name from uuid using above map
+          cookings.forEach((cooking) => {
+            if (programName.hasOwnProperty(cooking.programId)) {
+              cooking.programName = programName[cooking.programId];
+            }
+          });
+
+          res.send(cookings);
+          next()
+        }
+      });
     }
-    next()
   });
 }
